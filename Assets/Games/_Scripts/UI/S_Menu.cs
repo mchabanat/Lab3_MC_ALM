39bd3c8941ee621cff08b664f26c9535e34ce688
Arthur Le Menn <arthurlemenn29870@gmail.com>
2024-04-07 12:54:06 -0400

    Feat : Ajout des settings dans le menu

--- Assets/Games/_Scripts/UI/S_Menu.cs
+++ Assets/Games/_Scripts/UI/S_Menu.cs
@@ -5,6 +5,13 @@ using UnityEngine.UIElements;
 
 public class S_Menu : MonoBehaviour
 {
+    VisualElement menuContainer;
+    VisualElement settingsContainer;
+
+    DropdownField dropDownScreenMode;
+    DropdownField dropDownScreenResolution;
+    Slider volumeSlider;
+
     private void OnEnable()
     {
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
@@ -13,10 +20,51 @@ public class S_Menu : MonoBehaviour
         Button settingsButton = root.Q<Button>("buttonSettings");
         Button quitButton = root.Q<Button>("buttonQuit");
 
+        Button menuButton = root.Q<Button>("menuButton");
+        Button applyButton = root.Q<Button>("applyButton");
+
+        dropDownScreenMode = root.Q<DropdownField>("dropDownScreenMode");
+        dropDownScreenResolution = root.Q<DropdownField>("dropDownScreenSize");
+        volumeSlider = root.Q<Slider>("volumeSlider");
+
+        menuContainer = root.Q<VisualElement>("ContainerMenu");
+        settingsContainer = root.Q<VisualElement>("ContainerSettings");
+
+        menuContainer.style.display = DisplayStyle.Flex;
+        settingsContainer.style.display = DisplayStyle.None;
+
+        dropDownScreenResolution.choices = new List<string> { "1920x1080", "1280x720", "800x600" };
+        dropDownScreenMode.choices = new List<string> { "Windowed", "Fullscreen" };
+
+        if (Screen.fullScreen)
+        {
+            dropDownScreenMode.value = "Fullscreen";
+        }
+        else
+        {
+            dropDownScreenMode.value = "Windowed";
+        }
+
+        if(Screen.width == 1920 && Screen.height == 1080)
+        {
+            dropDownScreenResolution.value = "1920x1080";
+        }
+        else if (Screen.width == 1280 && Screen.height == 720)
+        {
+            dropDownScreenResolution.value = "1280x720";
+        }
+        else
+        {
+            dropDownScreenResolution.value = "800x600";
+        }
+
+
         startButton.clicked += () => play();
         settingsButton.clicked += () => settings();
         quitButton.clicked += () => quit();
 
+        menuButton.clicked += () => backToMenu();
+        applyButton.clicked += () => applySettings();
     }
 
     private void play()
@@ -25,10 +73,30 @@ public class S_Menu : MonoBehaviour
     }
     private void settings()
     {
-        Debug.Log("Settings");
+        menuContainer.style.display = DisplayStyle.None;
+        settingsContainer.style.display = DisplayStyle.Flex;
     }
     private void quit()
     {
-        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    private void backToMenu()
+    {
+        menuContainer.style.display = DisplayStyle.Flex;
+        settingsContainer.style.display = DisplayStyle.None;
+    }
+
+    private void applySettings()
+    {
+        if (dropDownScreenMode.value == "Fullscreen")
+        {
+            Screen.fullScreen = true;
+        }
+        else
+        {
+            Screen.fullScreen = false;
+        }
+        Screen.SetResolution(int.Parse(dropDownScreenResolution.value.Split('x')[0]), int.Parse(dropDownScreenResolution.value.Split('x')[1]), Screen.fullScreen);
     }
 }
